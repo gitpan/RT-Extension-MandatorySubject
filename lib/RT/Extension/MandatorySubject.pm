3ceba1d1a52aa637a8c2ea53a1d8818c13f0a7d2
Emmanuel Lacour <elacour@cpan.org>
2009-07-03 10:14:29 +0000

    Import of ELACOUR/RT-Extension-MandatorySubject-0.02 from CPAN.
    gitpan-cpan-distribution: RT-Extension-MandatorySubject
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ELACOUR/RT-Extension-MandatorySubject-0.02.tar.gz
    gitpan-cpan-author:       ELACOUR
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/MandatorySubject.pm
+++ lib/RT/Extension/MandatorySubject.pm
@@ -9,11 +9,11 @@ RT::Extension::MandatorySubject - Enforce users to fill the subject when creatin
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
