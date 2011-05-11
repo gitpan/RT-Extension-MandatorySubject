1c9871e0af0cf41403505595b3794edfe8f0ad9c
Emmanuel Lacour <elacour@cpan.org>
2011-05-11 15:39:47 +0000

    Import of ELACOUR/RT-Extension-MandatorySubject-0.04 from CPAN.
    gitpan-cpan-distribution: RT-Extension-MandatorySubject
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         ELACOUR/RT-Extension-MandatorySubject-0.04.tar.gz
    gitpan-cpan-author:       ELACOUR
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/MandatorySubject.pm
+++ lib/RT/Extension/MandatorySubject.pm
@@ -9,11 +9,11 @@ RT::Extension::MandatorySubject - Enforce users to fill the subject when creatin
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
@@ -69,7 +69,7 @@ L<http://search.cpan.org/dist/RT-Extension-MandatorySubject>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Emmanuel Lacour, all rights reserved.
+Copyright 2008-2011 Emmanuel Lacour, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
